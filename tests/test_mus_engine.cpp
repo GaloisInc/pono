27cf88b5639896e7d4dea3c59b7886e7c09bce40
Ryan Peroutka <ryan.peroutka@galois.com>
2024-03-07 00:39:59 -0600

    add c example from btor2 paper

--- tests/test_mus_engine.cpp
+++ tests/test_mus_engine.cpp
@@ -34,6 +34,7 @@ const vector<tuple<string, int, int>> quiip_models_unsat({
   // BTOR2
   tuple("test-models-btor2/count2/count2.btor2", 5, 1),
   tuple("test-models-btor2/count2/count2mus.btor2", 5, 2),
+  tuple("test-models-btor2/btor2_paper_c_example/example.btor2", 5, 1),
   // SMV
   tuple("test-models-smv/altitude_switch_model/altitude_switch_model.smv", 5, 2),
   tuple("test-models-smv/simple_counter/simple_counter.smv", 5, 1),
@@ -43,7 +44,8 @@ const vector<tuple<string, int, int>> quiip_models_unsat({
 
 const vector<tuple<string, int>> quiip_models_btor2_sat({
   tuple("test-models-btor2/count2/count2.btor2", 10),
-  tuple("test-models-btor2/count2/count2mus.btor2", 10)
+  tuple("test-models-btor2/count2/count2mus.btor2", 10),
+  tuple("test-models-btor2/btor2_paper_c_example/example.btor2", 10)
 });
 
 TEST_P(MusEngineTestsUnsat, Unsat)
