7434f8836028b922111a0b6441b4541132049d8b
Ryan Peroutka <ryan.peroutka@galois.com>
2024-03-06 23:09:07 -0600

    update test paths

--- tests/test_mus_engine.cpp
+++ tests/test_mus_engine.cpp
@@ -35,10 +35,10 @@ const vector<tuple<string, int, int>> quiip_models_unsat({
   tuple("test-models-btor2/count2/count2.btor2", 5, 1),
   tuple("test-models-btor2/count2/count2mus.btor2", 5, 2),
   // SMV
-  tuple("test-models-smv/altitude_switch_model.smv", 5, 2),
-  tuple("test-models-smv/simple_counter.smv", 5, 1),
-  tuple("test-models-smv/count2.smv", 5, 1),
-  tuple("test-models-smv/count2mus.smv", 5, 2)
+  tuple("test-models-smv/altitude_switch_model/altitude_switch_model.smv", 5, 2),
+  tuple("test-models-smv/simple_counter/simple_counter.smv", 5, 1),
+  tuple("test-models-smv/count2/count2.smv", 5, 1),
+  tuple("test-models-smv/count2/count2mus.smv", 5, 2)
 });
 
 const vector<tuple<string, int>> quiip_models_btor2_sat({
