52116741ba43499b9234f33294021487453c7844
Ryan Peroutka <ryan.peroutka@galois.com>
2024-03-13 16:20:59 -0500

    add some documentation and TODOs

--- engines/mus.cpp
+++ engines/mus.cpp
@@ -13,8 +13,8 @@ namespace pono {
       throw invalid_argument("MUS engine requires the --logging-smt-solver flag");
     }
     engine_ = Engine::MUS_ENGINE;
-    // TODO - what does `full_model` do?
-    boolector = create_solver_for(BTOR, BMC,false, true);
+    // TODO(rperoutka) - what does `full_model` do?
+    boolector = create_solver_for(BTOR, BMC, false, true);
     toBoolectorInternal = new TermTranslator(boolector);
   }
 
@@ -23,6 +23,7 @@ namespace pono {
     controlTerms.clear();
   }
 
+  // TODO(rperoutka) - Master.enumerate `exit(1)`s on satisfiable instances.
   ProverResult Mus::check_until(int k)
   {
     Master m(buildMusQuery(k));
@@ -107,33 +108,61 @@ namespace pono {
     return conjuncts;
   }
 
+
+  /*
+   * Construct a MUST Master object initialized with our MUS query by making
+   * assertions to a BoolectorSolver object, dumping smt2 from the BoolectorSolver,
+   * and passing the resulting smt2 off to MUST.
+   *
+   * The constraint set we set up for MUST to reason over consists of
+   *  - `INIT` constraints (top-level conjuncts of TransitionSystem.init())
+   *  - `TRANS` constraints (top-level conjuncts of TransitionSystem.trans())
+   *  - `INVAR` constraints (elements of TransitionSystem.constraints())
+   *  - `SPEC` the negation of the safety property
+   *
+   * For each element of the constraint set, we introduce a new symbol ("control
+   * variable"). The CONTROL_TERMS constraint stipulates that for all control
+   * variables, cv, cv holds iff its corresponding constraint holds.
+   *
+   * The approach of introducing new symbols and tying them to together with
+   * CONTROL_TERMS is necessary to prevent BoolectorSolver from rewriting away
+   * our intended constraint structure.
+   *
+   */
   Master Mus::buildMusQuery(int k)
   {
+
 
     UnorderedTermSet initConjuncts = extractTopLevelConjuncts(ts_.init());
     UnorderedTermSet transConjuncts = extractTopLevelConjuncts(ts_.trans());
 
+    /*
+     *  Constraints are populated in the TransitionSystem for SMV `INVAR`s and
+     *  BTOR2 `constraint`s. They're automatically added to both the
+     *  TransitionSystem's init and trans conjunctions. We'd like to treat these
+     *  as atomic constraints in our MUSes.
+     */
     UnorderedTermMap nextMap;
     for(auto &v: ts_.statevars()) {
       nextMap.insert({v, ts_.next(v)});
     }
-
     UnorderedTermSet constraints;
     for (auto & c: ts_.constraints()) {
       assert(c.second);
       constraints.insert(c.first);
     }
-
     for (auto &c: constraints) {
-      if (initConjuncts.find(c) != initConjuncts.end()) {
-        initConjuncts.erase(c);
-      }
-      if (transConjuncts.find(c) != transConjuncts.end()) {
-        transConjuncts.erase(c);
-        transConjuncts.erase(solver_->substitute(c, nextMap));
-      }
+      initConjuncts.erase(c);
+      transConjuncts.erase(c);
+      transConjuncts.erase(solver_->substitute(c, nextMap));
     }
 
+    /*
+     * If the conjunct encodes a state update constraint (i.e. of form
+     * <stateVar>.next = ...), then uses that state variable's id for the
+     * control var symbol. Otherwise use the conjunct's to_string as an
+     * identifier.
+     */
     UnorderedTermMap transIdToConjunct;
     for (auto &tc: transConjuncts) {
       Term id = tc;
@@ -144,12 +173,6 @@ namespace pono {
         }
       }
       transIdToConjunct.insert({id, unrollUntilBound(tc, k)});
-    }
-
-    for(auto &c: ts_.constraints()) {
-      if (initConjuncts.find(c.first) != initConjuncts.end()) {
-        initConjuncts.erase(c.first);
-      }
     }
 
     for (auto &ic: initConjuncts) {
@@ -167,14 +190,12 @@ namespace pono {
       makeControlEquality(cv, unrollUntilBound(c.first, k + 1));
     }
 
-    // SPEC
     Term spec = orig_property_.prop();
     logger.log(0, "Checking Spec: {}", spec);
     Term specCv = makeControlVar(ConstraintType::SPEC, spec);
     Term negSpec = boolector->make_term(PrimOp::Not, unrollUntilBound(spec, k + 1));
     makeControlEquality(specCv, negSpec);
 
-    // CONTROL TERMS
     Term ctCv = makeControlVar(ConstraintType::CONTROL_TERMS);
     Term ce = makeControlEquality(ctCv, makeConjunction(controlTerms));
     boolector->assert_formula(ce);
@@ -184,20 +205,29 @@ namespace pono {
       boolector->assert_formula(cv);
     }
 
+    // TODO(rperoutka) - make query output location configurable
     string musQueryFile = ".musquery.smt2";
     boolector->dump_smt2(musQueryFile);
 
     boolectorAliasCleanup(musQueryFile);
     // MUST defaults to remus when alg isn't specified on CLI
+    // TODO(rperoutka) - make alg configurable via CLI flag
     return Master(musQueryFile, "remus");
   }
 
   TermVec Mus::musAsOrigTerms(MUS mus)
   {
-    TermVec terms;
-    // Skip control terms assertion
+    // All MUSes should contain the CONTROL_TERMS infrastructure:
     assert(mus.bool_mus[0]);
-    for(int i = 1; i < mus.bool_mus.size(); i++) {
+    assert(mus.bool_mus[mus.bool_mus.size() - 1]);
+
+    TermVec terms;
+    /*
+     * MUS elements at indices 0 and (mus.bool_mus.size() - 1) are the
+     * CONTROL_TERMS infrastructure tying all other control vars to their
+     * constaints. Don't include them in the returned MUS.
+     */
+    for(int i = 1; i < mus.bool_mus.size() - 1; i++) {
       if (mus.bool_mus[i]) {
           terms.push_back(controlVars.at(i-1));
       }
@@ -211,6 +241,16 @@ bool ends_with(std::string const & value, std::string const & ending)
     return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
   }
 
+  /*
+   *  Boolector aliases `Bool` and `(_ BitVec 1)` internally. Its
+   *  boolector_dump_smt2 method appears to attempt to explicit covert between
+   *  the two in the resulting smt2, but there appears to be a bug when it's
+   *  dealing with `select`s yeilding a `(_ BitVec 1)`.
+   *
+   *  This function explictly converts `select`s used as
+   *    - conditions for `ite`s
+   *    - the sole term in a fun returing `Bool`.
+   */
   void Mus::boolectorAliasCleanup(string fname)
   {
     std::ifstream smtQuery(fname);
@@ -224,8 +264,6 @@ bool ends_with(std::string const & value, std::string const & ending)
         getline(stream2, l);
         lines.push_back(l);
     }
-
-    //lines[0] = "(set-logic AUFBV)";
 
     for (int i = 1; i < lines.size(); i++) {
         std::string line = lines[i];
@@ -266,6 +304,10 @@ bool ends_with(std::string const & value, std::string const & ending)
     bool skip = true;
     for(auto &l: lines) {
       if (skip) {
+        // BoolectorSolver.dump_smt2 adds a `(set-logic QF_UFBV)` on the first
+        // line. Boolector seems to accept the use of arrays within this, but
+        // Z3 does not. Just delete the line erronous `set-logic` for now.
+        // TODO(rperoutka) - perhaps swap it to `(set-logic QF_AUFBV)`? Is there a performance benefit to this?
         skip = false;
       } else {
         out << l << std::endl;
