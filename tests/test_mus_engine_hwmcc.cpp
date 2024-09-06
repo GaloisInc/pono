20e3928bf4867aa147970435fbf12a67b4751efb
Ryan Peroutka <ryan.peroutka@galois.com>
2024-09-06 11:27:00 -0500

    Update MUS engine HWMCC test cases to assume a flat dir of test vectors

--- tests/test_mus_engine_hwmcc.cpp
+++ tests/test_mus_engine_hwmcc.cpp
@@ -26,46 +26,76 @@ class MusEngineSameSatResult : public ::testing::Test,
 {
 };
 
-const string HWMCC = std::string(getenv("QUIIP_MODELS_ROOT")) + "/test/btor2/hwmcc-benchmarks/";
+const string HWMCC = std::string(getenv("QUIIP_MODELS_ROOT")) + "/vectors/hwmcc";
 
 vector<string> getBenchmarks()
 {
   const vector<string> testDirs = {
-    // = Bit Vector =
-    "bv/2019/beem",
-    "bv/2019/goel/crafted",
-    "bv/2019/goel/opensource",
-    // "bv/2019/goel/industry", // Very slow due to use of large bitvectors
-    "bv/2019/wolf/2019B",
-    "bv/2019/mann/safe",
-    "bv/2019/mann/unknown",
-    "bv/2019/mann/unsafe",
-    "bv/2020/mann",
+    // == Bit Vector ==
+    "anderson.3.prop1-back-serstep.btor2",
+    "brp2.2.prop1-func-interl.btor2",
+    "elevator.4.prop1-func-interl.btor2",
+    "mcs.3.prop1-back-serstep.btor2",
+    "pgm_protocol.3.prop5-func-interl.btor2",
+    "at.6.prop1-back-serstep.btor2",
+    "brp2.3.prop1-back-serstep.btor2",
+    "frogs.5.prop1-func-interl.btor2",
+    "msmie.3.prop1-func-interl.btor2",
+    "pgm_protocol.7.prop1-back-serstep.btor2",
+    "blocks.4.prop1-back-serstep.btor2",
+    "brp2.6.prop3-back-serstep.btor2",
+    "krebs.3.prop1-func-interl.btor2",
+    // DISABLED - very slow for bound > 2
+    // "peg_solitaire.3.prop1-back-serstep.btor2",
+    "rushhour.4.prop1-func-interl.btor2",
+    "paper_v3.btor2",
+    "h_RCU.btor2",
+    "h_TreeArb.btor2",
+    "miim.btor2",
+    "vcegar_arrays_itc99_b12_p2.btor2",
+    "vcegar_QF_BV_ar.btor2",
+    "vcegar_QF_BV_itc99_b13_p10.btor2",
+    "vis_arrays_am2901.btor2",
+    "vis_arrays_am2910_p1.btor2",
+    "vis_arrays_am2910_p2.btor2",
+    "vis_arrays_am2910_p3.btor2",
+    "vis_arrays_buf_bug.btor2",
+    "vis_arrays_bufferAlloc.btor2",
+    "intersymbol_analog_estimation_convergence.btor",
+    // DISABLED - slow
+    // "ridecore.btor",
+    // DISABLED - slow
+    // "buggy_ridecore.btor",
+    "marlann_compute_cp_fail1-p2.btor",
+    "marlann_compute_cp_fail2-p0.btor",
+    "marlann_compute_cp_pass-p2.btor",
+    "rast-p00.btor",
+    "rast-p03.btor",
+    "rast-p06.btor",
+    "rast-p14.btor",
+    "rast-p17.btor",
+    "rast-p19.btor",
+    "simple_alu.btor",
+    "stack-p2.btor",
+    "rast-p01.btor",
+    "rast-p04.btor",
+    "rast-p11.btor",
+    "rast-p16.btor",
+    "rast-p18.btor",
+    "rast-p21.btor",
+    "stack-p1.btor",
 
-    // = Array =
-    "array/2019/mann/safe",
-    //
-    // "array/2019/mann/unsafe",
-    // "array/2019/mann/unknown",
-    // "array/2019/wolf/2019B",
-    "array/2020/mann"
-  };
-  const vector<string> ignore = {
-
-    // very slow for bound > 2
-    HWMCC + "bv/2019/beem/peg_solitaire.3.prop1-back-serstep.btor2",
-
-    // slow
-    HWMCC + "bv/2019/mann/unknown/ridecore.btor",
-    HWMCC + "bv/2019/mann/unsafe/buggy_ridecore.btor",
+    // == Array ==
+    "arbitrated_fifos_n2d8w8.btor",
+    "array_swap.btor",
+    "easy_zero_array.btor",
+    "simple-stack-pred1.btor"
   };
   vector<string> benchmarks;
   for (const auto & testDir: testDirs) {
-    for (const auto & entry : std::filesystem::recursive_directory_iterator(fs::path(HWMCC + testDir))) {
+    for (const auto & entry : std::filesystem::directory_iterator(fs::path(HWMCC + testDir))) {
       if (entry.is_regular_file()) {
-        if (std::find(ignore.begin(), ignore.end(), entry.path().string()) == ignore.end()) {
-          benchmarks.push_back(entry.path());
-        }
+        benchmarks.push_back(entry.path());
       }
     }
   }
